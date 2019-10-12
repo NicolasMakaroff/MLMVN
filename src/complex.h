70212f3eea50560d11c1a35140e76e68e65f42a4
Nicolas_Makaroff <nicolas.makaroff@sfr.fr>
2019-10-12 12:27:48 +0200

    add complex operator

--- src/complex.h
+++ src/complex.h
@@ -1,3 +1,5 @@
+#include<cstdio>
+
 #if !defined(__COMPLEX_H)
 #define __COMPLEX_H
 
@@ -36,55 +38,6 @@ namespace complex_number{
                 };
 
         template<typename _Tp>
-        Complex<_Tp> operator+(const Complex<_Tp>&,const int){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator+(const Complex<_Tp>&, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator+(const int, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator-(const Complex<_Tp>&, const int){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator-(const Complex<_Tp>&, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator-(const int,const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator*(const Complex<_Tp>&, const int){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator*(const Complex<_Tp>&, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator*(const int, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator/(const Complex<_Tp>&, const int){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator/(const Complex<_Tp>&, const Complex<_Tp>&){
-
-        }
-        template<typename _Tp>
-        Complex<_Tp> operator/(const int, const Complex<_Tp>&){
-
-        }
-
-        template<typename _Tp>
         Complex<_Tp> &Complex<_Tp>::operator+=(const Complex<_Tp>& _z){
                 real_ += _z.real_;
                 imag_ += _z.imag_;
@@ -115,12 +68,106 @@ namespace complex_number{
 
         template<typename _Tp>
         Complex<_Tp> &Complex<_Tp>::operator/=(const Complex<_Tp>& _z){
-                const _Tp _z1 = 1;
+                const _Tp _z1 = real_ * _z.real_ + imag_ * _z.imag_;
+                const _Tp _n = _z.real_ * _z.real_ + _z.imag_ * _z.imag_;
+                imag_ = (imag_ * _z.real_ - real_ * _z.imag_)/_n;
+                real_ = _z1 / _n;
+                return *this;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> &Complex<_Tp>::operator/=(const _Tp t){
+                real_ /= t;
+                imag_ /= t;
+                return *this;
+        }
+
+
+
+        template<typename _Tp>
+        Complex<_Tp> operator+(const Complex<_Tp>& _z,const _Tp t ){
+                Complex<_Tp> _z1 = _z;
+                _z1 += t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator+(const Complex<_Tp>& _z1, const Complex<_Tp>& _z2){
+                Complex<_Tp> _z3 = _z1;
+                _z3 += _z2;
+                return _z3;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator+(const int t, const Complex<_Tp>& _z){
+                Complex<_Tp> _z1 = _z;
+                _z1 += t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator-(const Complex<_Tp>& _z, const int t){
+                Complex<_Tp> _z1 = _z;
+                _z1 -= t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator-(const Complex<_Tp>& _z1, const Complex<_Tp>& _z2){
+                Complex<_Tp> _z3 = _z1;
+                _z3 -= _z2;
+                return _z3;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator-(const int t,const Complex<_Tp>& _z){
+                Complex<_Tp> _z2 = _z;
+                _z2 -= t;
+                return _z2;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator*(const Complex<_Tp>& _z, const int t){
+                Complex<_Tp> _z1 =_z;
+                _z1 *= t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator*(const Complex<_Tp>& _z1, const Complex<_Tp>& _z2){
+                Complex<_Tp> _z3 = _z1;
+                _z3 *= _z2;
+                return _z3;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator*(const int t, const Complex<_Tp>& _z){
+                Complex<_Tp> _z1 = _z;
+                _z1 *= t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator/(const Complex<_Tp>& _z, const int t){
+                Complex<_Tp> _z1 = _z;
+                _z1 /= t;
+                return _z1;
+        }
+
+        template<typename _Tp>
+        Complex<_Tp> operator/(const Complex<_Tp>& _z1, const Complex<_Tp>& _z2){
+                Complex<_Tp> _z3 = _z1;
+                _z3 /= _z2;
+                return _z3;
+        }
+        template<typename _Tp>
+        Complex<_Tp> operator/(const int t, const Complex<_Tp>& _z){
+                Complex<_Tp> _z1;
+                _z1 /= t;
+                return _z1;
         }
 
 }
-
-
 
 
 #endif // __COMPLEX_H
